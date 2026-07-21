d52dde7c07dc9bda4d1a206990fe7c8c8bf81b19
DevMoutawakil <massarmostapha332@gmail.com>
2026-07-21 21:28:07 +0100

    Refactor CatalogueView constructor for clarity

--- src/main/java/com/cinema/view/CatalogueView.java
+++ src/main/java/com/cinema/view/CatalogueView.java
@@ -11,14 +11,13 @@ public class CatalogueView {
 
     public CatalogueView() {
         root = new ScrollPane();
-        root.setFitToWidth(true); // Adapte la largeur à l'écran
-        // On rend le fond transparent pour ne pas cacher ta belle image rouge
+        root.setFitToWidth(true);
         root.setStyle("-fx-background-color: transparent; -fx-background: transparent;");
 
         grid = new FlowPane();
         grid.setPadding(new Insets(30));
-        grid.setHgap(25); // Espace horizontal entre les cartes
-        grid.setVgap(25); // Espace vertical entre les cartes
+        grid.setHgap(25);
+        grid.setVgap(25);
         grid.setAlignment(Pos.TOP_LEFT);
         grid.setStyle("-fx-background-color: transparent;");
 
